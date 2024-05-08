5c4fb10537193cb26bf8ee1004310c1b2f52e09c
Kzsanjeet <sanjeetkazithapa@gmail.com>
2024-05-08 23:17:01 +0545

    added index.js

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = 4000; 
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+app.listen(PORT, () => { 
+    console.log(`Server is running on the port ${PORT}`);
+});
